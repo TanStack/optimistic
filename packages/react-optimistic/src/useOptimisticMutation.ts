5ffc936a55317144b3faf5932cf6222ee43121d7
Kyle Mathews <mathews.kyle@gmail.com>
2025-05-07 14:15:04 -0600

    Fix types

--- packages/react-optimistic/src/useOptimisticMutation.ts
+++ packages/react-optimistic/src/useOptimisticMutation.ts
@@ -1,14 +1,14 @@
 import { createTransaction } from "@tanstack/optimistic"
-import type { TransactionConfig } from "@tanstack/optimistic"
+import type { Transaction, TransactionConfig } from "@tanstack/optimistic"
 
 export function useOptimisticMutation(config: TransactionConfig) {
   return {
-    mutate: (callback: () => {}) => {
+    mutate: (callback: () => void): Transaction => {
       const transaction = createTransaction(config)
       transaction.mutate(callback)
       return transaction
     },
-    createTransaction: () => {
+    createTransaction: (): Transaction => {
       return createTransaction({ ...config, autoCommit: false })
     },
   }
