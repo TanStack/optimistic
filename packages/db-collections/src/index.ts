1a78479aab55f38195ce25e3117d21a47d4e6202
Kyle Mathews <mathews.kyle@gmail.com>
2025-05-09 16:05:52 -0600

    Add initial implementation of QueryCollection

--- packages/db-collections/src/index.ts
+++ packages/db-collections/src/index.ts
@@ -3,3 +3,9 @@ export {
   createElectricCollection,
   type ElectricCollectionConfig,
 } from "./electric"
+export {
+  QueryCollection,
+  createQueryCollection,
+  type QueryCollectionConfig,
+  type QueryKey, // Also exporting QueryKey as it's part of QueryCollectionConfig
+} from "./query"
